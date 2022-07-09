a6243f023c790cbecedc660575bbe2d49c31b672
Zyaden Foo <108923538+zayden2022@users.noreply.github.com>
2022-07-09 11:15:43 +0300

    Create 1342-number-of-steps-to-reduce-a-number-to-zero.py

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
+    
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        count = 0
+        while num > 0:
+            if num % 2 == 1:
+                num -= 1
+            else:
+                num //= 2
+            count += 1
+        return count
